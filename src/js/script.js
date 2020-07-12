eb35e2099865e01620688150c18e92994c90092a
Eeveeee <fx8300gfe@gmail.com>
2020-07-12 21:08:47 +0300

    some fixes

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#nav").addEventListener("click", (e) => {
-    e.preventDefault();
-    let anchor = e.target;
-    let id = anchor.getAttribute("href");
-    document.querySelector(id).scrollIntoView({
-      block: "start",
-      behavior: "smooth",
-    });
+    if (e.target.getAttribute("class") === "navbar__block") {
+      e.preventDefault();
+      let anchor = e.target;
+      let id = anchor.getAttribute("href");
+      document.querySelector(id).scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+      });
+    }
   });
 });
