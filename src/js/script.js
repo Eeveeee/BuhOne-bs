79cfaa30724b0d30ae6f44ee6363b4c7fe07d84b
Eeveeee <fx8300gfe@gmail.com>
2020-07-14 12:12:37 +0300

    burger-menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
+const nav = document.getElementById("nav");
+const burgerMenu = document.querySelector(".burger-menu");
+
+function widthCheck() {
+  if (document.documentElement.clientWidth >= 480) return;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#nav").addEventListener("click", (e) => {
     if (e.target.getAttribute("class") === "navbar__block") {
@@ -8,6 +15,21 @@ document.addEventListener("DOMContentLoaded", () => {
         block: "start",
         behavior: "smooth",
       });
+      widthCheck();
+      nav.style.left = "-100vw";
+      burgerMenu.classList.toggle("burger-active");
     }
   });
 });
+
+burgerMenuClick();
+
+function burgerMenuClick() {
+  widthCheck();
+  nav.style.left = "-100vw";
+  burgerMenu.addEventListener("click", (e) => {
+    e.preventDefault();
+    burgerMenu.classList.toggle("burger-active");
+    nav.style.left = nav.style.left === "-100vw" ? 0 : "-100vw";
+  });
+}
