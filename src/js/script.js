dc8a09a0f49a6e53ea39023dd3ca34bfaec23ce5
Eeveeee <fx8300gfe@gmail.com>
2020-08-21 22:37:27 +0300

    anchor fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,56 +1,50 @@
-const nav = document.getElementById("nav");
-const burgerMenu = document.querySelector(".burger-menu");
+const nav = document.getElementById('nav')
+const burgerMenu = document.querySelector('.burger-menu')
 
 function widthCheck() {
-  if (document.documentElement.clientWidth >= 480) return 0;
+  if (document.documentElement.clientWidth >= 480) return 0
 }
 
 function scrollState(state) {
-  if (state === false) {
-    document.body.style = "overflow:hidden";
-  } else {
-    document.body.style = "overflow:auto";
-  }
+  !state
+    ? (document.body.style = 'overflow:hidden')
+    : (document.body.style = 'overflow:auto')
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#nav").addEventListener("click", (e) => {
-    if (e.target.getAttribute("class") === "navbar__block") {
-      e.preventDefault();
-      let anchor = e.target;
-      let id = anchor.getAttribute("href");
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#nav').addEventListener('click', (e) => {
+    if (e.target.getAttribute('class') === 'navbar__block') {
+      e.preventDefault()
+      let anchor = e.target
+      let id = anchor.getAttribute('href')
       document.querySelector(id).scrollIntoView({
-        block: "start",
-        behavior: "smooth",
-      });
+        block: 'start',
+        behavior: 'smooth',
+      })
       if (widthCheck()) {
-        return;
+        return false
       }
-      if (burgerMenu.classList.contains("burger-active")) {
-        scrollState(true);
-      } else {
-        scrollState(false);
-      }
-      nav.style.left = "-100vw";
-      burgerMenu.classList.toggle("burger-active");
+      scrollState(true)
+      nav.style.left = '-100vw'
+      burgerMenu.classList.toggle('burger-active')
     }
-  });
-});
+  })
+})
 
-burgerMenuClick();
+burgerMenuClick()
 function burgerMenuClick() {
   if (widthCheck()) {
-    return;
+    return false
   }
-  nav.style.left = "-100vw";
-  burgerMenu.addEventListener("click", (e) => {
-    if (burgerMenu.classList.contains("burger-active")) {
-      scrollState(true);
+  nav.style.left = '-100vw'
+  burgerMenu.addEventListener('click', (e) => {
+    if (burgerMenu.classList.contains('burger-active')) {
+      scrollState(true)
     } else {
-      scrollState(false);
+      scrollState(false)
     }
-    e.preventDefault();
-    burgerMenu.classList.toggle("burger-active");
-    nav.style.left = nav.style.left === "-100vw" ? 0 : "-100vw";
-  });
+    e.preventDefault()
+    burgerMenu.classList.toggle('burger-active')
+    nav.style.left = nav.style.left === '-100vw' ? 0 : '-100vw'
+  })
 }
